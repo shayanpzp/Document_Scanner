a0818a6557ce3b3d7050a93e3f57e025f67664be
unknown <shayanpzp>
2023-09-30 22:07:32 +0330

    Commit 4 : add method processing

--- Scanner.py
+++ Scanner.py
@@ -1,16 +1,33 @@
 import cv2
+import numpy as np
 
 
-frame_width = 640
-frame_height = 480
+width_image = 640
+hight_image = 480
 
 capture = cv2.VideoCapture(0)
-capture.set(3,frame_width)
-capture.set(4,frame_height)
+capture.set(3,width_image)
+capture.set(4,hight_image)
 capture.set(10,150)
+
+
+def processing(image):
+    kernel = np.ones((5,5))
+
+    image_gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+    image_blur = cv2.GaussianBlur(image_gray, (5,5), 1)
+    image_canny = cv2.Canny(image_blur, 200, 200)
+    image_dialation = cv2.dilate(image_canny, kernel, iterations=2)
+    image_threshold = cv2.erode(image_dialation, kernel, iterations=1)
+
+    return image_threshold
+
+
 
 while True:
     success , image = capture.read()
+    image = cv2.resize(image,(width_image,hight_image))
+    image_threshold =  processing(image)
     cv2.imshow("Video Capture", image)
     if cv2.waitKey(1) and 0xFF == ord('q'):
         break
