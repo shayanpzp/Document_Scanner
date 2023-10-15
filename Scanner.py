fe1269a39f99a1371b0f47cbec64e7c22f3d9579
shayanpzp <shayanpzp1@gmail.com>
2023-10-15 21:39:53 +0330

    Commit 5 : add method contours and reprocessing image

--- Scanner.py
+++ Scanner.py
@@ -11,6 +11,7 @@ capture.set(4,hight_image)
 capture.set(10,150)
 
 
+
 def processing(image):
     kernel = np.ones((5,5))
 
@@ -24,10 +25,39 @@ def processing(image):
 
 
 
+
+def contours(img):
+    biggest = np.array([])
+    max_area = 0
+    contours,hierarchy = cv2.findContours(img,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+
+    for cnt in contours:
+
+        area = cv2.contourArea(cnt)
+
+        if area>5000:
+
+            cv2.drawContours(image_contour, cnt, -1, (255, 0, 0), 3)
+            peri = cv2.arcLength(cnt,True)
+            approx = cv2.approxPolyDP(cnt,0.02*peri,True)
+
+            if area > max_area and len(approx) == 4:
+                biggest = approx
+                max_area = area
+
+    return biggest
+
+
+
+
 while True:
     success , image = capture.read()
     image = cv2.resize(image,(width_image,hight_image))
+    image_contour = image.copy()
     image_threshold =  processing(image)
+    contours(image_threshold)
+
     cv2.imshow("Video Capture", image)
+
     if cv2.waitKey(1) and 0xFF == ord('q'):
         break
